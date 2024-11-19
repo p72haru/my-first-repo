ac343356429d96ab02c87acae28a256bb4086757
p72haru <p72haru.webcreator@gmail.com>
2024-11-19 21:36:01 +0900

    ABOUT,SERVICEセクションの流れるテキストアニメーションの実装

--- js/script.js
+++ js/script.js
@@ -1,44 +1,43 @@
-  // モバイルメニュー
-  const hamburger = document.querySelector(".hamburger");
-  const closeIconWrap = document.querySelector(".close-icon__wrap");
-  const nav = document.querySelector(".nav");
-  const mask = document.querySelector(".mask");
-  const logo = document.querySelector(".logo__image");
+// モバイルメニュー
+const hamburger = document.querySelector(".hamburger");
+const closeIconWrap = document.querySelector(".close-icon__wrap");
+const nav = document.querySelector(".nav");
+const mask = document.querySelector(".mask");
+const logo = document.querySelector(".logo__image");
 
-  // メニューを開く関数
-  function openMenu() {
-    closeIconWrap.style.display = "block";
-    nav.style.display = "block";
-    hamburger.style.display = "none";
-    mask.style.display = "block";
-    logo.style.opacity = "0.7";
+// メニューを開く関数
+function openMenu() {
+  closeIconWrap.style.display = "block";
+  nav.style.display = "block";
+  hamburger.style.display = "none";
+  mask.style.display = "block";
+  logo.style.opacity = "0.7";
+}
+
+// メニューを閉じる関数
+function closeMenu() {
+  nav.style.display = "none";
+  hamburger.style.display = "block";
+  closeIconWrap.style.display = "none";
+  mask.style.display = "none";
+  logo.style.opacity = "1";
+}
+
+// イベント設定
+hamburger.addEventListener("click", openMenu);
+closeIconWrap.addEventListener("click", closeMenu);
+mask.addEventListener("click", closeMenu);
+
+// リサイズ時のdisplayリセット
+window.addEventListener("resize", () => {
+  if (window.innerWidth >= 1200) {
+    hamburger.style.display = "";
+    closeIconWrap.style.display = "";
+    nav.style.display = "";
+    mask.style.display = "";
+    logo.style.opacity = "";
   }
-
-  // メニューを閉じる関数
-  function closeMenu() {
-    nav.style.display = "none";
-    hamburger.style.display = "block";
-    closeIconWrap.style.display = "none";
-    mask.style.display = "none";
-    logo.style.opacity = "1";
-  }
-
-  // イベント設定
-  hamburger.addEventListener("click", openMenu);
-  closeIconWrap.addEventListener("click", closeMenu);
-  mask.addEventListener("click", closeMenu);
-
-  // リサイズ時のdisplayリセット
-  window.addEventListener("resize", () => {
-    if (window.innerWidth >= 1200) {
-      hamburger.style.display = "";
-      closeIconWrap.style.display = "";
-      nav.style.display = "";
-      mask.style.display = "";
-      logo.style.opacity = "";
-    }
-  });
-
+});
 
 // FAQアコーディオン
 document.addEventListener("DOMContentLoaded", () => {
